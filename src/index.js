69e067cb1004b37eac2629d7a420fe2324cd81f2
Kelly Castelo <kawokkun@gmail.com>
2022-06-02 17:19:09 -0300

    Métodos

--- src/index.js
+++ src/index.js
@@ -4,9 +4,47 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    return res.json({message: 'haro wordu'})
+/*
+    GET - BUSCAR INFORMAÇÕES DENTRO DO SERVIDOR
+    POST - INSERIR UMA INFORMAÇÃO NO SERVIDOR
+    PUT - ALTERAR UMA INFORMAÇÃO NO SERVIDOR
+    PATCH - ALTERAR UMA INFORMAÇÃO ESPECÍFICA NO SERVIDOR
+    DELETE - DELETAR UMA INFORMAÇÃO NO SERVIDOR
+*/
+
+const cursos = [
+    "Curso 1",
+    "Curso 2",
+    "Curso 3"
+]
+
+app.get('/courses', (req, res) => {
+    return res.json(cursos);
 })
+
+app.post('/courses', (req, res) => {
+    const response  = req.body.curso; 
+    cursos.push(response);
+})
+
+app.put('/courses/:id', (req, res) => {
+    const {id} = req.params;
+    const response  = req.body.curso; 
+    
+})
+
+app.patch('/courses/:id', (req, res) => {
+    const {id} = req.params;
+    const response  = req.body.curso; 
+    
+})
+
+app.delete('/courses/:id', (req, res) => {
+    const {id} = req.params;
+    const response  = req.body.curso; 
+    
+})
+
 
 app.listen(PORT, () => {
     console.log('Server is up!');
