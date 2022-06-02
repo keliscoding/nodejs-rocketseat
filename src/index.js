f816f6266bad532b4f13788a6eac77b13218771b
Kelly Castelo <kawokkun@gmail.com>
2022-06-02 18:02:26 -0300

    Requisitos e Regras de Negócio

--- src/index.js
+++ src/index.js
@@ -22,41 +22,9 @@ const PORT = 3000;
     BODY PARAMS => OBJETOS DE INSERÇÃO / ALTERAÇÃO
 */
 
-
-const cursos = [
-    "Curso 1",
-    "Curso 2",
-    "Curso 3"
-]
-
-app.get('/courses', (req, res) => {
-    return res.json(cursos);
-})
-
-app.post('/courses', (req, res) => {
-    const response  = req.body.curso; 
-    cursos.push(response);
-})
-
-app.put('/courses/:id', (req, res) => {
-    const {id} = req.params;
-    const response  = req.body.curso; 
-    
-})
-
-app.patch('/courses/:id', (req, res) => {
-    const {id} = req.params;
-    const response  = req.body.curso; 
-    
-})
-
-app.delete('/courses/:id', (req, res) => {
-    const {id} = req.params;
-    const response  = req.body.curso; 
-    
-})
-
-
 app.listen(PORT, () => {
     console.log('Server is up!');
 });
+
+
+
