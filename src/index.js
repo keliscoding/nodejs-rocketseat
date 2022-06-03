24ae6fdab1d245e445810b7b693b8ad20ce45114
Kelly Castelo <kawokkun@gmail.com>
2022-06-03 05:38:13 -0300

    listar extrato bancário por data

--- src/index.js
+++ src/index.js
@@ -112,6 +112,17 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
     return response.status(201).send();
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+    const {customer} = request;
+    const {date} = request.query;
+
+    const dateFormat = new Date(date + "00:00");
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return response.json(customer.statement);
+});
+
 
 app.listen(PORT, () => {
     console.log('Server is up!');
