1ea93550cda4d4f77d89b266164631e6567c29a1
Kelly Castelo <kawokkun@gmail.com>
2022-06-03 09:39:48 -0300

    criando repositório de categoria

---
+++ Chapter_2/RENTALX/src/repositories/Categories.Repository.ts
@@ -0,0 +1,26 @@
+import Category from "../model/Category";
+
+//DTO => Data transfer object, pegam o valor da rota e entregam ao repositório
+
+interface ICreateCategoryDTO {
+    name: string;
+    description: string;
+}
+
+export default class CategoriesRepository {
+    private categories: Category[];
+
+    constructor() {
+        this.categories = [];
+    }
+
+    create({ description, name }: ICreateCategoryDTO): void {
+        const category = new Category({
+            description,
+            name,
+            created_at: new Date(),
+        });
+
+        this.categories.push(category);
+    }
+}
