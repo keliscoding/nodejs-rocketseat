bf35906ef85f06ede21d5cbfad02c9de986672eb
Kelly Castelo <kawokkun@gmail.com>
2022-06-03 10:52:11 -0300

    Utilizando o principio de responsabilidade unica

--- Chapter_2/RENTALX/src/routes/categories.routes.ts
+++ Chapter_2/RENTALX/src/routes/categories.routes.ts
@@ -1,19 +1,14 @@
 import { Router } from "express";
 import CategoriesRepository from "../repositories/Categories.Repository";
+import { CreateCategoryService } from "../services/Categories/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
-
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-    if(categoryAlreadyExists) {
-        return response.status(400).json({error: "categoria já existe!"})
-    }
-
-    categoriesRepository.create({ name, description });
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    createCategoryService.execute({name, description});
 
     return response.status(201).send();
 });
---
+++ Chapter_2/RENTALX/src/services/Categories/CreateCategoryService.ts
@@ -0,0 +1,35 @@
+import CategoriesRepository from '../../repositories/Categories.Repository';
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+/*
+    [x] - Definir o tipo de retorno
+    [x] - Alterar o retorno de erro
+    [x] - Acessar o repositório
+*/
+
+
+//Service não deve conhecer o tipo de repositório, service é alto nível
+//Principio de inversão de dependencia
+//
+class CreateCategoryService {
+    private categoriesRepository: CategoriesRepository;
+
+    constructor(categoriesRepository: CategoriesRepository) {
+        this.categoriesRepository = categoriesRepository;
+    }
+    execute({description, name}: IRequest): void {
+        const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+        if (categoryAlreadyExists) {
+            throw new Error('Category already exists!')
+        }
+
+        this.categoriesRepository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService };
