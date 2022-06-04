0d265f188bc5d0ed953e32cbb2a210b38e282d9d
Kelly Castelo <kawokkun@gmail.com>
2022-06-04 07:29:59 -0300

    4. Conhecendo o conceito de stream4.

--- Chapter_2/RENTALX/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Chapter_2/RENTALX/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,11 +1,24 @@
+import fs from "fs";
+import {parse} from 'csv-parse';
+
 interface IRequest {
-    file: any;
+    file: Express.Multer.File;
 }
 
 class ImportCategoryUseCase {
     constructor() {}
 
-    execute({file}: IRequest) {}
+    execute({file}: IRequest): void {
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = parse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on("data", async(line) => {
+            console.log(line)
+        });
+    }
 }
 
 export { ImportCategoryUseCase };
