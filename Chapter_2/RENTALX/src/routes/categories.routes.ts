0d64b76f8887d6733d4302e1031edc78c8da4ad1
Kelly Castelo <kawokkun@gmail.com>
2022-06-04 06:59:15 -0300

    2. Criando upload de arquivos

--- Chapter_2/RENTALX/src/routes/categories.routes.ts
+++ Chapter_2/RENTALX/src/routes/categories.routes.ts
@@ -2,11 +2,20 @@ import { Router } from "express";
 import CategoriesRepository from "../modules/cars/repositories/implementations/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import {  } from "../modules/cars/useCases/createCategory/CreateCategoryUseCase";
+import multer from "multer";
+
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
-
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+
+/*
+    pegar os arquivos do upload, salvar dentro de uma pasta temporária, fazer toda leitura e tudo que precisar, tipo 
+    salvar no banco de dados, e ai fazer a deleção dentro da pasta temporária
+
+*/
+const upload = multer({
+    dest: "./temp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -15,5 +24,14 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
 })
+
+// no imsonia, tem que colocar "multipart form" pra fazer o upload
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    const {file} = request;
+    return response.send();
+})
+
+
 
 export { categoriesRoutes };
