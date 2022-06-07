1da7023488961d1992d02aaaef442eb840f01ff1
Kelly Castelo <kawokkun@gmail.com>
2022-06-07 08:58:59 -0300

    10. Criando migrations especificação de carros(Many to Many)

--- Chapter_4/RENTALX/src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
+++ Chapter_4/RENTALX/src/modules/cars/useCases/listAvailableCars/ListAvailableCarsUseCase.ts
@@ -1,7 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
-import { AppError } from '../../../../shared/errors/AppError';
 
 interface IRequest {
     category_id?: string;
